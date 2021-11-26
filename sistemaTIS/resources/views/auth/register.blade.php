fc4d43dce49344606ef7e1062b7153bdf48bae7b
Jhon Ponce <poncejhon321@gmail.com>
2021-11-26 14:43:32 -0400

    actualizacion de forms de registro

--- sistemaTIS/resources/views/auth/register.blade.php
+++ sistemaTIS/resources/views/auth/register.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container" >
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+            <div class="card" >
+                <div class="card-header" style="background-color: #e3342f; color: white; ">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
@@ -106,11 +106,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-dark" >
                                     {{ __('Register') }}
                                 </button>
+                                <a href="/" class="btn btn-dark">
+                                    Cancelar
+                                </a>
                             </div>
-                        </div>
                     </form>
                 </div>
             </div>
