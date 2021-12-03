1d6c79743502a859130216cbfa63e0098c4a2b5f
Jhon Ponce <poncejhon321@gmail.com>
2021-12-03 14:29:50 -0400

    creacion de las vistas de editar y mostrar

---
+++ sistemaTIS/resources/views/convocatoria/show.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('template_title')
+    {{ $convocatoria->tit_conv ?? 'Ver convocatoria' }}
+@endsection
+
+@section('content')
+    <section class="content container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="float-left">
+                            <span class="card-title">Ver Convocatoria</span>
+                        </div>
+                        <div class="float-right">
+                            <a class="btn btn-primary" href="{{ route('convocatorias.index') }}"> Volver</a>
+                        </div>
+                    </div>
+
+                    <div class="card-body">
+
+                        
+                        <div class="form-group">
+                            <strong>Codigo de la convocatoria</strong>
+                            {{ $convocatoria->id }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Titulo de la convocatoria</strong>
+                            {{ $convocatoria->tit_conv }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Descripcion de la convocatoria</strong>
+                            {{ $empresa->desc_conv }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Fecha de publicacion</strong>
+                            {{ $empresa->created_at }}
+                        </div>
+                        <div class="form-group">
+                            <strong>Fecha de finalizacion</strong>
+                            {{ $empresa->f_fin }}
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
