2b67e7bc966099cff0a93c7806ef949ef71043be
Jhon Ponce <poncejhon321@gmail.com>
2021-12-08 19:24:02 -0400

    creacion de los controladores para los distintos roles

--- sistemaTIS/app/Http/Kernel.php
+++ sistemaTIS/app/Http/Kernel.php
@@ -63,8 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'Consultor' => \App\Http\Middleware\SoloAdmin::class,
-	    'soloUser' => \App\Http\Middleware\SoloUser::class,
-	    'soloGrupo' => \App\Http\Middleware\SoloModerador::class,
+        'consultor' => \App\Http\Middleware\Consultor::class,
+	    'solouser' => \App\Http\Middleware\SoloUser::class,
+	    'sologrupo' => \App\Http\Middleware\SoloGrupo::class,
     ];
 }
