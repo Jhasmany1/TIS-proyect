efd4bb4262e62c801502f67201c4835dc7e9340a
warris2 <92058571+warris2@users.noreply.github.com>
2021-12-22 00:06:42 -0400

    Add files via upload

--- sistemaTIS/routes/web.php
+++ sistemaTIS/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GrupoController;
-use App\Http\Controllers\UserController;
+use App\Http\Controller\SobreController;
+use App\Http\Controllers\EmpresaController;
+use App\Mail\NotificacionMailReceived;
+use Illuminate\Support\Facades\Mail;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,16 +21,50 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-/* Rutas de los usuarios--------------------------------------------------- */
+/* Rutas de los links--------------------------------------------------- */
+// Route::get('/addEmpresa',function(){
+//     return view('addEmpresa');
+    
+// })->name('addEmpresa');
 
-Route::resource('/users', UserController::class);
+// Route::get('/listEmpresas',function(){
+//      return view('listEmp');
+    
+// })->name('listEmpresa');
+// Route::get('/listEmpresas', App\Http\Controllers\CrearEmpresaController@index);
+// Route::post('empresas', App\Http\Controllers\CrearEmpresaController@store);
 
-Route::resource('/grupos', GrupoController::class);
+
+Route::get('/addPropuesta',function(){
+    return view('addPropuesta');
+});
+
+Route::get('/listaPropuesta',function(){
+    return view('listaPropuesta');
+});
+
 
 /*---------------------------------------------------------------------*/
 Auth::routes();
 
-Route::resource('/empresas',App\Http\Controllers\EmpresaController::class);
-Route::resource('/convocatorias',App\Http\Controllers\ConvocatoriaController::class);
+Route::resource('/empresas', App\Http\Controllers\EmpresaController::class);
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+/*-------------------------Rutas nuevas vistas-------------------------*/
+
+
+//Route::post('/subir','App\Http\Controllers\SobreBController@storeB')->name('subir');
+//Route::post('/subir','App\Http\Controllers\SobreAController@storeA')->name('subir');
+
+//Route::get('sobre_b_s','App\Http\Controllers\SobreBController@index');
+//Route::get('sobre_a_s','App\Http\Controllers\SobreAController@index');
+
+Route::post('/addPropuesta', [App\Http\Controllers\SobreController::class, 'store']);
+Route::get('/listaPropuesta', [App\Http\Controllers\SobreController::class, 'index']);
+
+
+
+/*-----------------------Ruta envio de correos---------------------------*/
+
+Route::post('listaPropuesta', [App\Http\Controllers\MensajeController::class, 'store']);
